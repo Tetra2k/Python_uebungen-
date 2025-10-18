7255da9df9470d747c486ca40bed68d706c88c08
Dominic <103526203+Tetra2k@users.noreply.github.com>
2025-10-18 13:05:20 +0000

    versuche hinzufügen

--- Krother/NummerRaten.py
+++ Krother/NummerRaten.py
@@ -2,15 +2,25 @@ import random
 
 zahl = random.randint(1, 100)
 
-player = input("Gib eine Zahl zwischen 1 und 100 ein: ")
+player = input("Gib eine Zahl zwischen 1 und 100 ein. Du hast 5 Versuche: ")
 richtig = False
+versuche = 4
+while not richtig and versuche > 0:
+        try: 
+            player = int(input("Gib eine Zahl zwischen 1 und 100 ein. Du hast noch {} Versuche: ".format(versuche)))
+        except ValueError:
+            print("Bitte gib eine gültige Zahl ein.")
+            continue
 
-while not richtig:
-    player = input("Gib eine Zahl zwischen 1 und 100 ein: ")
-    if zahl < int(player):
-        print("Die Zahl ist zu groß")
-    elif zahl > int(player):
-        print("Die Zahl ist zu klein")
-    else:
+        if zahl < player:
+            versuche -= 1
+            print("Die Zahl ist zu groß")
+        elif zahl > player:
+            versuche -= 1
+            print("Die Zahl ist zu klein")
+        else:
          print("Herzlichen Glückwunsch! Du hast die Zahl erraten.")
          richtig = True
+
+if not richtig:
+    print(f"Leider keine Versuche mehr. Die richtige Zahl war {zahl}.")
