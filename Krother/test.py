bf0ccd93186202da5c29ca67950b5435eafa1b21
Dominic <103526203+Tetra2k@users.noreply.github.com>
2025-10-18 13:07:49 +0000

    versuche hinzufuegen

--- Krother/test.py
+++
@@ -1,9 +0,0 @@
-customer_basket_cost = 34
-customer_basket_weight = 44
-
-# Write if statement here to calculate the total cost
-
-if customer_basket_cost < 100:
-   ship = customer_basket_weight *1.2
-   price = customer_basket_cost + ship
-   print(price)
